6a827d1eb5473f10e9e95c17d7894bd841d33d5f
Lean <basedlainpilled@gmail.com>
2023-04-20 01:42:40 -0400

    add world types

---
+++ src/world.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const WorldInfoSchema = z.object({
+  name: z.string().optional(),
+  description: z.string().optional(),
+  image: z.string().url().optional(),
+  host: z.string().min(1).optional(),
+  extras: z.record(z.any()).optional().optional(),
+});
+
+export type WorldInfo = z.infer<typeof WorldInfoSchema>;
+
+export const WorldMetadataSchema = z.object({
+  info: WorldInfoSchema.optional(),
+  model: z.string().min(1),
+});
+
+export type WorldMetadata = z.infer<typeof WorldMetadataSchema>;
