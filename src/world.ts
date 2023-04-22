516d3237f618e7401db291c8682b2a0492a98296
Lean <basedlainpilled@gmail.com>
2023-04-22 01:29:21 -0400

    update schema for multiple authors

--- src/world.ts
+++ src/world.ts
@@ -3,7 +3,14 @@ import { z } from "zod";
 export const WorldInfoSchema = z.object({
   name: z.string().optional(),
   description: z.string().optional(),
-  author: z.string().optional(),
+  authors: z
+    .array(
+      z.object({
+        name: z.string().optional(),
+        address: z.string().optional(),
+      })
+    )
+    .optional(),
   image: z.string().url().optional(),
   host: z.string().min(1).optional(),
   extras: z.record(z.any()).optional(),
