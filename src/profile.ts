3ea080df86c0c28092a0eaa866bf68b25d61feb9
Lean <basedlainpilled@gmail.com>
2023-04-27 22:30:14 -0400

    add handle to profile

--- src/profile.ts
+++ src/profile.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const ProfileMetadataSchema = z.object({
   name: z.string().optional(),
+  handle: z.string().optional(),
   bio: z.string().optional(),
   image: z.string().optional(),
   background: z.string().optional(),
